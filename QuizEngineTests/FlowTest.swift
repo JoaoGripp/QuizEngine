1f70e8319ed628cabd973b9b5942df8f796228ea
JoaoGripp <corguinha@gmail.com>
2022-08-21 06:52:09 -0300

    finish the Flow

--- QuizEngineTests/FlowTest.swift
+++ QuizEngineTests/FlowTest.swift
@@ -66,6 +66,25 @@ class FlowTest: XCTestCase {
 
         XCTAssertEqual(router.routedQuestions, ["Q1"])
     }
+//    MARK: - Testing Answer
+    
+    func test_start_withNoQuestions_doesNotRouteToResult() {
+        makeSUT(questions: []).start()
+        
+        XCTAssertEqual(router.routedResult!, [:])
+    }
+    
+    func test_startAndAnswerFirstAndSecondQuestion_withTwoQuestions_routesToResult() {
+        let sut = makeSUT(questions: ["Q1", "Q2"])
+        sut.start()
+        
+        router.answerCallBack("A1")
+        router.answerCallBack("A2")
+
+        XCTAssertEqual(router.routedResult!, ["Q1" : "A1", "Q2" : "A2"])
+    }
+    
+    
     
 //    MARK: - Helpers
     
@@ -75,11 +94,17 @@ class FlowTest: XCTestCase {
     
     class RouterSpy: Router {
         var routedQuestions: [String] = []
+        var routedResult: [String: String]? = nil
+        
         var answerCallBack: Router.AnswerCallback = { _ in }
         
         func routeTo(question: String, answerCallBack: @escaping AnswerCallback) {
             routedQuestions.append(question)
             self.answerCallBack = answerCallBack
+        }
+        
+        func routeTo(result: [String : String]) {
+            routedResult = result
         }
     }
 }
